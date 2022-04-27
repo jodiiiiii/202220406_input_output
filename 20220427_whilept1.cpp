6767013d44ece57f6292a6a65f574ab807c767ae
jodiiiiii <103085992+jodiiiiii@users.noreply.github.com>
2022-04-27 10:17:29 +0800

    Create 20220427_whilept1.cpp

---
+++ 20220427_whilept1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int number1 = 0;
+    while (number1 ++ <10){//perulangan turun
+       cout<<"1"<<"*"<<number1<<" = "<<number1*1<<endl;
+   }
+   cout<<endl<<endl;
+   int number2 = 0;
+   while (number2 ++ <10){//perulangan turun
+       cout<<"2"<<"*"<<number2<<" = "<<number2*2<<endl;
+   }
+    cout<<endl<<endl;
+    int number3 = 0;
+   while (number3 ++ <10){//perulangan turun
+       cout<<"3"<<"*"<<number3<<" = "<<number3*3<<endl;
+   }
+   cout<<endl<<endl;
+   int number4 = 0;
+   while (number4 ++ <10){//perulangan turun
+       cout<<"4"<<"*"<<number4<<" = "<<number4*4<<endl;
+   }
+   cout<<endl<<endl;
+   int number5 = 0;
+   while (number5 ++ <10){//perulangan turun
+       cout<<"5"<<"*"<<number5<<" = "<<number5*5<<endl;
+   }
+   cout<<endl<<endl;
+   int number6= 0;
+   while (number6 ++ <10){//perulangan turun
+       cout<<"6"<<"*"<<number6<<" = "<<number6*6<<endl;
+   }
+    return 0;
+}
